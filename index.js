bb06e2f206a002147bc722200c136a743175a73e
Your Name <domtuz@yahoo.com>
2023-10-04 17:40:24 -0400

    Edited Problem 6 - Added triple sum

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ console.log(multiply(3, 7.5))
 
 //6
 const add = (num1,num2) => {
-    return num1 + num2;
+    return num1 === num2 ? (num1 + num2) * 3 : num1 + num2;
 }
 console.log(add(2, 4));
 console.log(add(10, -5));
 console.log(add(3, 7.5));  
+console.log(add(5, 5)); 
